726579b389a0ae863b26493208395aae6b221a74
Karan Kumar <kkumar04600@gmail.com>
2026-02-25 15:23:22 +0530

    Add analytics module placeholder for future implementation

---
+++ analytics.py
@@ -0,0 +1,16 @@
+# Analytics Module - Future Implementation
+# This module will track link usage statistics
+
+def track_link_access(share_id):
+    """Track when a link is accessed"""
+    # TODO: Implement analytics tracking
+    pass
+
+def get_link_statistics(share_id):
+    """Get statistics for a specific link"""
+    # TODO: Implement statistics retrieval
+    return {
+        'total_views': 0,
+        'unique_visitors': 0,
+        'last_accessed': None
+    }
